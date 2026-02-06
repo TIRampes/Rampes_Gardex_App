b984c2317960aa320009d0842dc599218dc36589
TI <ti@rampesgardex.com>
2026-02-06 08:51:29 -0500

    add utils folder

---
+++ app/utils/codesMapping.ts
@@ -0,0 +1,21 @@
+
+export const codesAchat = {
+  A_FAIRE: '①',
+  FAIT: '√',
+  RECEPTIONNE: 'R',
+  PRET_A_RAMASSER: 'P',
+  BACK_ORDER: 'B/O'
+};
+
+export const codesProduction = {
+  COMPLETE: '√',
+  ATTENTE_CLIENT: 'At.C',
+  NON_APPLICABLE: 'N/A',
+  PARTIEL: 'P',
+  DOSSIER_MESUREUR: 'D',
+  MODIFICATION: 'M',
+  ATTENTE_CAROL_CONFIRM: 'C-C',
+  ATTENTE_CAROL_MESURE: 'C-RM',
+  BACK_ORDER: 'B/O',
+  ATTENTE_REPRESENTANT: 'At. Rep'
+};
