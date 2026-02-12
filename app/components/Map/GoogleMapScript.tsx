198c45b0748a251dbb9b6dcc01b1b7303e8c6c52
TI <ti@rampesgardex.com>
2026-02-12 12:21:39 -0500

    map issue

--- app/components/Map/GoogleMapScript.tsx
+++ app/components/Map/GoogleMapScript.tsx
@@ -2,11 +2,12 @@
 
 import Script from "next/script";
 
-export default function GoogleMapsScript() {
+export default function GoogleMapScript() {
   return (
     <Script
+      id="google-maps-script"
       src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-      strategy="beforeInteractive"
+      strategy="afterInteractive"
     />
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Providers from "./providers";
+import GoogleMapsScript from "./components/Map/GoogleMapScript";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>{children}</Providers>
+        <GoogleMapsScript />
       </body>
     </html>
   );
