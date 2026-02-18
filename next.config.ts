314b00a19f93830a1d4317ec11fde81fa9c97af4
TI <ti@rampesgardex.com>
2026-02-18 14:44:56 -0500

    end

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-   output: "standalone"
+  /* config options here */
 };
 
 export default nextConfig;
